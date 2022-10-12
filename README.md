fc7e2264824e2fdc76ec3c16c4d35b6597346ba5
Maynard <me@maynards.site>
2022-10-11 20:42:11 -0700

    generalize API to simply "(lib, state) -> itemList"

--- README.md
+++ README.md
@@ -7,9 +7,59 @@ Just another application launcher. Looks something like this:
 
 ## Why?
 
-The primary gimmick of Nifty is that it's configured via Javascript file instead of configuration file. See `example/`.
+The primary gimmick of Nifty is that it's configured via Javascript module instead of configuration file.
 
-The real reason I made Nifty is that I don't really like any existing launchers!
+This means that as use-cases of Nifty get more complex, the complexity of the API will only increase a little bit in response. This is a big improvement over the status quo of "starting simple" with a standard config file and eventually drowning in complexity trying to shoehorn in more and more features. (See, for instance, Typescript's `tsconfig.json`.) By choosing Javascript as its "config format", Nifty gives the user a massive amount of flexibility, power, and expresiveness.
+
+You use `nifty` by calling it with a path to your configuration module, eg `nifty /path/to/config.js`. The provided module is expected to define a global `exports` object of the type `Exports` specified below. Also see `example/`.
+
+```
+type Item =
+  { text :: String
+      -- ^ Item text
+  , exec :: () -> ()
+      -- ^ What to do when the item is selected
+  , render :: ({ isSelected :: Bool }) -> Node
+      -- ^ Render to a DOM node
+  , isSticky :: Bool
+      -- ^ Sticks to top of sort
+  }
+
+type Lib =
+
+  -- | Built-in fuzzy sort
+  { sort :: Array Item -> Array Item
+
+  -- | Execute a bash string
+  , exec :: String -> ()
+
+  -- | Make a standard item
+  , mkItem ::
+      { text :: String
+      , exec :: () -> ()
+      , icon [optional] :: String
+          -- ^ Icon URI
+      }
+      -> Item
+
+  -- | Make a sticky item
+  , mkHeadline ::
+      { text :: String
+      , exec :: () -> ()
+      }
+      -> Item
+  }
+
+type Client =
+   ( Lib
+   , String
+       -- ^ Search query
+   )
+  -> Array Item
+       -- ^ What items to display
+
+type Exports = { default :: Client }
+```
 
 
 ## How to run it
--- example/example.js
+++ example/example.js
@@ -1,49 +1,70 @@
-const items = [];
-exports.items = items;
+exports.default = client;
 
-const proc = require('child_process');
 const plib = require('path');
 
-// Run a bash command
-function exec(str) {
-  const child = proc.spawn(
-    '/usr/bin/env',
-    ['bash', '-c', str],
-    {
-      detached: true,
-      stdio: ['ignore', 'inherit', 'inherit'],
+function client(
+  lib,   // possibly-useful utilities
+  query, // current search query
+) {
+
+  // Our job is to build the list of search results
+  // Call it 'items'
+  let items = [];
+
+  // First, we'll populate it with some standard items
+
+  // A firefox launcher
+  items.push(lib.mkItem({
+    text: 'Firefox',
+    exec: function() {
+      // On select, run the "firefox" command
+      lib.exec('firefox');
+    },
+    icon: 'file://' + plib.resolve(__dirname, './icons/firefox.png'),
+  }));
+
+  // A chrome launcher, too; why not?
+  items.push(lib.mkItem({
+    text: 'Google Chrome',
+    exec: function() {
+      lib.exec('google-chrome');
+    },
+    icon: 'file://' + plib.resolve(__dirname, './icons/chrome.png'),
+  }));
+
+  // A dummy item
+  items.push(lib.mkItem({
+    text: 'Dummy item -- will error',
+    exec: function() {
+      throw 'oopsies';
+    },
+  }));
+
+  // If the query starts with an equals, we'll interpret
+  // it as a javascript expression to evaluate.
+  // Occasionally handy to, perhaps, quickly calculate sums
+  if (query.startsWith('=')) {
+    const expr = query.slice(1);
+
+    let result;
+    try {
+      result = eval(expr);
+    } catch (e) {
+      // Will happen under normal circumstances, eg if
+      // the user types '=5+' on the way to typing '=5+3'.
+      // As such, ignore an error.
     }
-  );
-  child.unref();
+
+    items.push(lib.mkHeadline({
+      text: result ? (result + '') : '',
+      exec: () => {},  // do nothing on select
+    }));
+  }
+
+  // Sort the items according to the query
+  items = lib.sort(items, query);
+
+  // Return!
+  return items;
+
 }
-
-items.push({
-  text: 'Firefox',
-  exec: function() {
-    exec('firefox');
-  },
-  icon: 'file://' + plib.resolve(__dirname, './icons/firefox.png'),
-});
-
-items.push({
-  text: 'Google Chrome',
-  exec: function() {
-    exec('google-chrome-stable');
-  },
-  icon: 'file://' + plib.resolve(__dirname, './icons/chrome.png'),
-});
-
-items.push({
-  text: 'Screenshot',
-  exec: function() {
-    exec('put-your-screenshot-command-here');
-  },
-  icon: 'file://' + plib.resolve(__dirname, './icons/screenshot.webp'),
-});
-
-items.push({
-  text: 'Will error',
-  exec: function() {
-    exec('oh-no-bad-command');
-  },
-});
