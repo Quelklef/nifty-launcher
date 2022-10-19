70d45d7a34c750a6629e77d76d10fbe5d6ee90de
Maynard <me@maynards.site>
2022-10-19 15:28:13 -0700

    use js entrypoint instead of html

---
+++ main.js
@@ -0,0 +1,10 @@
+nw.Window.open('index.html', {
+  id: "nifty",
+  title: "Nifty!",
+  always_on_top: true,
+  transparent: true,
+  frame: false,
+  width: 800,
+  height: 600,
+  resizable: false,
+});
